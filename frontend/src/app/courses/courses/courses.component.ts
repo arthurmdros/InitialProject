51aa90bd497ff4275fa8ee243b03938faa9b56d0
arthurmdros <arthurmedeiros2005@gmail.com>
2022-03-01 16:31:33 -0300

    Adding method to show dialog if error ocurred

--- frontend/src/app/courses/courses/courses.component.ts
+++ frontend/src/app/courses/courses/courses.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { MatDialog } from '@angular/material/dialog';
+import { catchError, Observable, of } from 'rxjs';
 import { Course } from '../model/course';
 import { CoursesService } from '../services/courses.service';
+import { ErrorDialogComponent } from '../../shared/components/error-dialog/error-dialog.component';
 
 @Component({
   selector: 'app-courses',
@@ -13,8 +15,22 @@ export class CoursesComponent implements OnInit {
   courses$: Observable<Course[]>;
   displayedColumns = ["name", "category"];
 
-  constructor(private  coursesService: CoursesService) {
-    this.courses$ = this.coursesService.findAll();
+  constructor(
+    private  coursesService: CoursesService,
+    public dialog: MatDialog
+    ) {
+    this.courses$ = this.coursesService.findAll().pipe(
+      catchError(error => {
+        this.onErrorDialog("Erro ao carregar dados dos cursos!");
+        return of([])
+      })
+    );
+   }
+
+   onErrorDialog(errorMsg: string){
+     this.dialog.open( ErrorDialogComponent,{
+       data: errorMsg 
+     });
    }
 
   ngOnInit(): void {  }
