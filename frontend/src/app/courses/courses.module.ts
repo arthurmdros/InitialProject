b6725c9834cbf026f09b4450beed82cb619071a2
arthurmdros <arthurmedeiros2005@gmail.com>
2022-03-01 13:27:21 -0300

    Adding new import to shared material

--- frontend/src/app/courses/courses.module.ts
+++ frontend/src/app/courses/courses.module.ts
@@ -1,10 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { MatTableModule } from '@angular/material/table';
 
+import { AppMaterialModule } from '../shared/app-material/app-material.module';
 import { CoursesRoutingModule } from './courses-routing.module';
 import { CoursesComponent } from './courses/courses.component';
-import {MatCardModule} from '@angular/material/card';
 
 
 @NgModule({
@@ -14,8 +13,7 @@ import {MatCardModule} from '@angular/material/card';
   imports: [
     CommonModule,
     CoursesRoutingModule,
-    MatTableModule,
-    MatCardModule
+    AppMaterialModule
   ]
 })
 export class CoursesModule { }
