fcb2a055da7e94be2616e04bcaf2889664850e38
arthurmdros <arthurmedeiros2005@gmail.com>
2022-03-01 14:04:26 -0300

    Adding httpClient into services

--- frontend/src/app/courses/services/courses.service.ts
+++ frontend/src/app/courses/services/courses.service.ts
@@ -1,11 +1,13 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
 import { Course } from '../model/course';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoursesService {
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
 
   findAll(): Course[] {
     return [
