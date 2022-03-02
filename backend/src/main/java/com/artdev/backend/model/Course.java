29b3cc1ae881587ca87a5256a6b4948f47d30348
arthurmdros <arthurmedeiros2005@gmail.com>
2022-03-02 12:57:43 -0300

    Adding property jackson to "_id"

--- backend/src/main/java/com/artdev/backend/model/Course.java
+++ backend/src/main/java/com/artdev/backend/model/Course.java
@@ -3,6 +3,8 @@ package com.artdev.backend.model;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -15,6 +17,7 @@ public class Course {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO )
+    @JsonProperty("_id")
     private Long id;
 
     @Column(length = 200, nullable = false)
