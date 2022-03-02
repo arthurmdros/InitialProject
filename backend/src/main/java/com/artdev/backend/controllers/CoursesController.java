aa0d5ad373d0bc777f09faa13a4e925d6bfcf682
arthurmdros <arthurmedeiros2005@gmail.com>
2022-03-02 13:01:34 -0300

    Adding routes to listOne course and save a new

--- backend/src/main/java/com/artdev/backend/controllers/CoursesController.java
+++ backend/src/main/java/com/artdev/backend/controllers/CoursesController.java
@@ -2,10 +2,14 @@ package com.artdev.backend.controllers;
 
 import java.util.List;
 
+import com.artdev.backend.dto.CourseDto;
 import com.artdev.backend.model.Course;
 import com.artdev.backend.services.CoursesService;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,4 +27,14 @@ public class CoursesController {
     public @ResponseBody List<Course> list() {
         return service.findAll();
     }
+
+    @GetMapping(value = "/{id}")
+    public @ResponseBody Course listOne(@PathVariable Long id) {
+        return service.findOne(id);
+    }
+
+    @PostMapping
+	public CourseDto insert(@RequestBody CourseDto dto) {		
+		return service.savePessoa(dto);		        
+	}
 }
