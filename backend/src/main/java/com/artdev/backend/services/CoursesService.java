8279c1c1b83746c6af5125d0973240aa954e87f5
arthurmdros <arthurmedeiros2005@gmail.com>
2022-03-02 12:55:41 -0300

    Adding class service to courses with the list all

---
+++ backend/src/main/java/com/artdev/backend/services/CoursesService.java
@@ -0,0 +1,23 @@
+package com.artdev.backend.services;
+
+import java.util.List;
+
+import org.springframework.transaction.annotation.Transactional;
+
+import com.artdev.backend.model.Course;
+import com.artdev.backend.repository.CourseRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CoursesService {
+    @Autowired
+	private CourseRepository repository;
+	
+	@Transactional(readOnly = true)
+	public List<Course> findAll(){
+        return repository.findAll();
+	}
+	
+}
