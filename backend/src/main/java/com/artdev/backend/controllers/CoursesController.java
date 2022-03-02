84829b024ce0534386349e6465f18fe3c7cd9269
arthurmdros <arthurmedeiros2005@gmail.com>
2022-03-02 11:37:05 -0300

    Adding route to acess list of all courses

--- backend/src/main/java/com/artdev/backend/controllers/CoursesController.java
+++ backend/src/main/java/com/artdev/backend/controllers/CoursesController.java
@@ -1,15 +1,26 @@
 package com.artdev.backend.controllers;
 
+import java.util.List;
+
+import com.artdev.backend.model.Course;
+import com.artdev.backend.repository.CourseRepository;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.AllArgsConstructor;
+
 @RestController
-@RequestMapping("/")
+@RequestMapping("/api/courses")
+@AllArgsConstructor
 public class CoursesController {
 
+    private final CourseRepository courseRepository;
+
     @GetMapping
-    public String hello(){
-        return "Hello for courses API!";
+    public  List<Course> list() {
+        return courseRepository.findAll();
     }
+
 }
